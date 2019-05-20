0c5e5cd0ce3c05661b13d4bc3c6708297f95244d
ponyaid <moskvichovoleg@gmail.com>
2019-05-20 12:43:39 +0300

    heroku deployment changes

--- shorturl.py
+++ shorturl.py
@@ -1,14 +1,8 @@
-from flask_script import Manager
-from flask_migrate import Migrate
-
 from app import create_app
-from app.database import db
 
 
 app = create_app()
 
-manager = Manager(app)
-migrate = Migrate(app, db)
 
 
 
